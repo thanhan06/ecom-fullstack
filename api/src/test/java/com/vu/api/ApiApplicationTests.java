78df1a35d988f2aaa3e8278a464a9389958f927a
thanhan06 <nguyenvanvu060104@gmail.com>
2026-04-02 21:21:30 +0700

    chore: init monorepo web/api + postgres compose

---
+++ api/src/test/java/com/vu/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vu.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
